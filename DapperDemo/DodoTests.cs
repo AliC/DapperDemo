3eff76f8c24007b6e9ed71ce37f7f30323da845c
AlastairDanleyCox <alastair.danley-cox@totaljobsgroup.com>
2017-03-02 13:33:22 +0000

    continued implementation of test - needs completion

--- DapperDemo/DodoTests.cs
+++ DapperDemo/DodoTests.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
 using NUnit.Framework;
 
 namespace DapperDemo
@@ -11,7 +15,7 @@ namespace DapperDemo
             _createTableScript += "\nGO\nCREATE TABLE dbo.GameStatistics (Id INT IDENTITY PRIMARY KEY, DodoId INT REFERENCES Dodo(Id), Bitepower INT, Cuteness INT, Speed INT)";
         }
 
-        [Test]
+        [Ignore("complete test")]
         public void GivenClassAssociation_WhenDataReadFromDatabase_DataMappedToMultipleObjects()
         {
             IList<Dodo> expectedDodos = TestData.GetDodos();
@@ -20,6 +24,22 @@ namespace DapperDemo
             string createDodoScript = TestData.GetInsertScriptFor(expectedDodos);
 
             ExecuteSqlForDatabase(createDodoScript);
+
+            using (IDbConnection connection = new SqlConnection(GetConnectionString(_databaseName)))
+            {
+                connection.Open();
+
+                actualDodos = connection.Query<Dodo>("SELECT * FROM dbo.Wombat").ToList();
+            }
+
+            Assert.That(actualDodos.Count, Is.EqualTo(4));
+
+            Assert.That(actualDodos[0].Id, Is.EqualTo(expectedDodos[0].Id));
+            Assert.That(actualDodos[0].Name, Is.EqualTo(expectedDodos[0].Name));
+            Assert.That(actualDodos[0].GameStatistics.BitePower, Is.EqualTo(expectedDodos[0].GameStatistics.BitePower));
+
+            Assert.That(actualDodos[1].Id, Is.EqualTo(expectedDodos[1].Id));
+            Assert.That(actualDodos[1].Name, Is.EqualTo(expectedDodos[1].Name));
         }
     }
 }
